ed8b525024f14795f98b9be8fd52fe5e4d41d07d
shreya-karna <shreyakarna98@gmail.com>
2026-08-13 11:49:50 +0545

    Add Inventory Page Object

---
+++ pages/inventory_page.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+
+
+class InventoryPage:
+
+    INVENTORY_CONTAINER = (By.ID, "inventory_container")
+    PRODUCT_ITEMS = (By.CLASS_NAME, "inventory_item")
+    CART_LINK = (By.CLASS_NAME, "shopping_cart_link")
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def is_inventory_displayed(self):
+        return self.driver.find_element(*self.INVENTORY_CONTAINER).is_displayed()
+
+    def get_product_count(self):
+        return len(self.driver.find_elements(*self.PRODUCT_ITEMS))
+
+    def click_cart(self):
+        self.driver.find_element(*self.CART_LINK).click()
